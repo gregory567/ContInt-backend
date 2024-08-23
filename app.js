de1aa4d215e963b1bc6f65d971deedf6dcfa691f
Simon Heider <simon.heider@gmx.at>
2024-08-23 17:23:54 +0200

    changed app.js file: changed origin ip in corsOptions in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { PostHog } = require('posthog-node');
 
 //configure cors 
 const corsOptions = {
-  origin: 'http://18.209.12.19:80',
+  origin: 'http://18.209.12.19/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
