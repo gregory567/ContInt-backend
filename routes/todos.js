240792d0bbb011f438277bcba244de960a265f78
Simon Heider <simon.heider@gmx.at>
2024-08-24 18:50:13 +0200

    reverted posthog changes in todos.js to default

--- routes/todos.js
+++ routes/todos.js
@@ -1,4 +1,26 @@
-const { body, validationResult } = require('express-validator');
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('todo', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        name: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        done: {
+            allowNull: false,
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+};
+
+/*const { body, validationResult } = require('express-validator');
 const db = require('../db/db');
 var express = require('express');
 var router = express.Router();
@@ -16,6 +38,7 @@ function getDistinctIdFromCookies(req) {
     }
     return null;  // Return null if there's an issue with the cookie or it's not found
 }
+
 
 // Helper function to handle todos sorting and date updating
 async function processTodos(todos, distinctId) {
@@ -126,3 +149,4 @@ router.delete('/:id/done', async (req, res, next) => {
 });
 
 module.exports = router;
+*/
