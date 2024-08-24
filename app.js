40a5400f4e720baa1cf0b1e7164808791a054432
Simon Heider <simon.heider@gmx.at>
2024-08-24 15:30:12 +0200

    removed logging in app.js

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ const posthog = new PostHog(
 
 
 var app = express();
-//logging
+/*logging
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
+*/
 
 //app.use(cors());
 app.use(cors(corsOptions)); //use cors with the corsoptions set above
