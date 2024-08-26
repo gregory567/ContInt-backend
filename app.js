9b6af354e10d542ad70f4246172d123314e5de53
Simon Heider <simon.heider@gmx.at>
2024-08-26 17:45:56 +0200

    added posthog to app.js and .json files again

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const { PostHog } = require('posthog-node');
 var todosRouter = require('./routes/todos');
+
+
+const posthog = new PostHog(
+  'phc_xC1fBU65c02AaFCisiKximyPseHTHIUGSRwtQayUXs0',
+  { host: 'https://eu.i.posthog.com' }
+);
 
 var app = express();
 app.use(cors());
