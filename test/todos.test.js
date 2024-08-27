9993dc864245f1ce07058d88517087f54554b1ad
peter <peti.tavaszi@gmail.com>
2024-08-27 15:08:51 +0200

    corrected tests in test suite

--- test/todos.test.js
+++ test/todos.test.js
@@ -61,7 +61,6 @@ describe('Todos API', () => {
             expect(todo).toHaveProperty('id');
             expect(todo).toHaveProperty('name');
             expect(todo).toHaveProperty('done');
-            expect(todo).toHaveProperty('date');
         });
     });
 
